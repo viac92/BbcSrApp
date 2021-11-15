58b4a2c2ba7f0334ca5b4664a8194450cfd6dd2b
Niccolo Baldini <nbaldini@bbctech.it>
2021-11-15 17:39:43 +0100

    miglioramento generale

--- BbcSrUI/Startup.cs
+++ BbcSrUI/Startup.cs
@@ -34,7 +34,7 @@ namespace BbcSrUI
         {
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+                    Configuration.GetConnectionString("Default")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddRazorPages();
