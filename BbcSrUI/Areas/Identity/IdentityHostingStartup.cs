fa2c76b31f4f1f720e8b893f60960964f44c1141
Niccolo Baldini <nbaldini@bbctech.it>
2021-11-18 10:27:49 +0100

    login impostata come prima pagina

---
+++ BbcSrUI/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using BbcSrUI.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(BbcSrUI.Areas.Identity.IdentityHostingStartup))]
+namespace BbcSrUI.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
