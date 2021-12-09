237a5df33451fa3fe0a82f9f5e053b67bf6ba7ee
Niccolo Baldini <nbaldini@bbctech.it>
2021-12-09 16:52:45 +0100

    migliorata login

--- BbcSrUI/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ BbcSrUI/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -51,7 +51,7 @@ namespace BbcSrUI.Areas.Identity.Pages.Account
             [DataType(DataType.Password)]
             public string Password { get; set; }
 
-            [Display(Name = "Remember me?")]
+            [Display(Name = "Ricordami")]
             public bool RememberMe { get; set; }
         }
 
